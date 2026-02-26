d4ecf3e215d368778dbe2f5b8c65dadb3b50e1fe
moomins07 <davidrsmith04@gmail.com>
2026-02-26 02:34:13 +0000

    added circleshape and player class modules

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 
@@ -12,15 +13,20 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.display.flip()
         screen.fill("black")
-
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+       
 
 
 
